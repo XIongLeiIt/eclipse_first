cd6c40515a07918936c2f07a90bdf74fd699e008
shilin <1843416717@qq.com>
2018-12-23 21:37:26 +0800

    git第一次提交

---
+++ First_JavaWeb/src/com/shilin/helloworld/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.shilin.helloworld;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello,world");
+		int f = 10;
+		System.out.println(f);
+	}
+
+}
